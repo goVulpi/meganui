90e6c76ef9542288b2211541199185fea5757098
Jimmy Andrade <jimmy.andrade@vulpi.com.br>
2018-12-04 16:05:43 -0200

    Added Image component

--- example/src/App.js
+++ example/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Button, Heading, AppContainer } from 'meganui';
+import { AppContainer, Button, Heading, Image } from 'meganui';
 
 export default class App extends Component {
   render () {
@@ -8,6 +8,7 @@ export default class App extends Component {
         <Button color="primary">Primary</Button>
         <Button color="secondary">Secondary</Button>
         <Heading level={2}>Hello</Heading>
+        <Image src="https://s3.amazonaws.com/signature-vulpi/pictures/Jimmy.png"/>
       </AppContainer>
     )
   }
