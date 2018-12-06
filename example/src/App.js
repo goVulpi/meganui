c932eff08d892cabd53b4cf02074edd171f03b25
Paulo Melo Jr <paulomelojunior@icloud.com>
2018-12-06 19:42:34 -0200

    Starting spacing component

--- example/src/App.js
+++ example/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { AppContainer, Button, Heading, Image, Position } from 'meganui';
+import { AppContainer, Button, Heading, Image, Position, Spacing } from 'meganui';
 import { Banner } from './Banner';
 import { Column } from './Column';
 import { Login } from './Login';
@@ -16,12 +16,25 @@ export default class App extends Component {
           </Column>
           <Column>
             <Login>
-              <Position center>
-                <Image height="72px" src="logotype.png"/>
-              </Position>
-              <Button size="big" color="brand" block>Entrar</Button>
-              <Button size="big" color="secondary" block>Entrar com LinkedIn</Button>
+              <Spacing position="top" size="auto">
+                <Position center>
+                  <Spacing position="bottom">
+                    <Image height="72px" src="logotype.png"/>
+                  </Spacing>
+                </Position>
+              </Spacing>
+              <Spacing position="bottom">
+                <Button size="big" color="brand" block>Entrar</Button>
+              </Spacing>
+              <Spacing position="bottom">
+                <Button size="big" color="secondary" block>Entrar com LinkedIn</Button>
+              </Spacing>
               <Button size="big" color="dark" block>Entrar com GitHub</Button>
+              <Spacing position="top" size="auto">
+                <small>
+                  Vulpi, feito com ❤ no San Pedro Valley, © 2018.
+                </small>
+              </Spacing>
             </Login>
           </Column>
         </Row>
--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,13 @@ import Button from './Button';
 import Heading from './Heading';
 import Image from './Image';
 import Position from './Position';
+import Spacing from './Spacing';
 
 // export Button and Heading as named exports
-export { AppContainer, Button, Heading, Image, Position }
+export { AppContainer, Button, Heading, Image, Position, Spacing }
 
 // alternative, more concise syntax for named exports
 // export { default as Foo } from './Foo'
 
 // set a default export for this module
-export default { AppContainer, Button, Heading, Image, Position }
+export default { AppContainer, Button, Heading, Image, Position, Spacing }
