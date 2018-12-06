9e32185d1e7efe6d78179422e2a4932d278acb46
Jimmy Andrade <jimmy.andrade@vulpi.com.br>
2018-12-06 20:30:40 -0200

    Text JSX to TSX

--- example/src/App.js
+++ example/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { AppContainer, Button, Heading, Image, TextInput } from 'meganui';
+import { AppContainer, Button, Heading, Image, Text, TextInput } from 'meganui';
 
 export default class App extends Component {
   render () {
@@ -10,6 +10,8 @@ export default class App extends Component {
         <Heading level={2}>Hello</Heading>
         <Image src="https://s3.amazonaws.com/signature-vulpi/pictures/Jimmy.png"/>
         <TextInput />
+        <Text>This is an inline text</Text>
+        <Text element="paragraph">This is a paragraph text</Text>
       </AppContainer>
     )
   }
--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,14 @@ import AppContainer from './AppContainer';
 import Button from './Button';
 import Heading from './Heading';
 import Image from './Image';
+import Text from './Text';
 import TextInput from './TextInput';
 
 // export Button and Heading as named exports
-export { AppContainer, Button, Heading, Image, TextInput }
+export { AppContainer, Button, Heading, Image, Text, TextInput }
 
 // alternative, more concise syntax for named exports
 // export { default as Foo } from './Foo'
 
 // set a default export for this module
-export default { AppContainer, Button, Heading, Image, TextInput }
+export default { AppContainer, Button, Heading, Image, Text, TextInput }
