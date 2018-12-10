f33053bd1a3bf5be83c5e035c97fb37417e0d6f9
Paulo Melo Jr <paulomelojunior@icloud.com>
2018-12-10 15:04:48 -0200

    Styles for email input

--- src/EmailInput/index.tsx
+++ src/EmailInput/index.tsx
@@ -1,15 +1,32 @@
 import * as React from 'react';
+import styles from './index.scss';
 
 export type Props = {
+    color: string,
     fieldId: string,
     labelledBy: string,
     hint: string,
     id: string,
     required: boolean,
-    requirementsText: string
+    requirementsText: string,
+    size: string
 };
 
 export default class EmailInput extends React.Component<Props> {
+
+    constructor(props: Props) {
+        super(props);
+    }
+
+    getClassNames() : string {
+        const { color, size } = this.props;
+        let classNames : string[] = [styles["email-input"]];
+
+        (typeof color === "string") ? classNames.push(styles[color]) : Function.prototype();
+        (typeof size === "string") ? classNames.push(styles[size]) : Function.prototype();
+
+        return classNames.join(" ");
+    }
 
     render() {
         const { hint, id, labelledBy, required, requirementsText } = this.props;
@@ -20,6 +37,7 @@ export default class EmailInput extends React.Component<Props> {
                 autoCorrect="off"
                 aria-labelledby={labelledBy}
                 aria-required={required}
+                className={this.getClassNames()}
                 maxLength={254}
                 minLength={3}
                 multiple={false}
